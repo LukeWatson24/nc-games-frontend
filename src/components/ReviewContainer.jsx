71df94a79a3e7ccb0d806ec8ef05876956a2cbb4
LukeWatson24 <watsonluke@sky.com>
2023-02-07 12:08:53 +0000

    desktop styling

--- src/components/ReviewContainer.jsx
+++ src/components/ReviewContainer.jsx
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
+import styles from "../styles/review.module.scss";
 import ReviewComments from "./ReviewComments";
 import ReviewInfo from "./ReviewInfo";
 
 function ReviewContainer() {
   const { review_id } = useParams();
   return (
-    <main>
+    <main className={styles.review_wrapper}>
       <ReviewInfo reviewId={review_id} />
       <ReviewComments reviewId={review_id} />
     </main>
