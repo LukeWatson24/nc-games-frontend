fcd54efb5da67b02e58005628d5c3ef694b5e065
LukeWatson24 <watsonluke@sky.com>
2023-02-07 10:56:29 +0000

    loading state added

--- src/components/ReviewsContainer.jsx
+++ src/components/ReviewsContainer.jsx
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import { getReviews } from "../utils/utils";
+import utils from "../styles/utils.module.scss";
 import Filter from "./Filter";
 import Reviews from "./Reviews";
 
 function ReviewsContainer() {
   const [reviews, setReviews] = useState([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   const [filter, setfilter] = useState({});
 
   useEffect(() => {
-    getReviews({ limit: 6 * page }).then((res) => setReviews(res));
+    setLoading(true);
+    getReviews({ limit: 6 * page }).then((res) => {
+      setReviews(res);
+      setLoading(false);
+    });
   }, [page]);
 
   return (
     <main>
       <Filter />
       <Reviews reviews={reviews} />
+      <p className={loading ? "" : utils.hidden}>Loading...</p>
       <button onClick={() => setPage((prev) => prev + 1)}>Load More</button>
     </main>
   );
