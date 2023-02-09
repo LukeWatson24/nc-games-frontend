28918bae036ca32f24a910147c9376d9e484e7b5
LukeWatson24 <watsonluke@sky.com>
2023-02-09 11:07:57 +0000

    sort reviews

--- src/components/ReviewsContainer.jsx
+++ src/components/ReviewsContainer.jsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react";
 import { getReviews } from "../utils/utils";
 import utils from "../styles/utils.module.scss";
-import Filter from "./Filter";
 import Reviews from "./Reviews";
+import Sort from "./Sort";
 
 function ReviewsContainer() {
   const [reviews, setReviews] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [filter, setfilter] = useState({});
+  const [sort, setSort] = useState("created_at");
+  const [order, setOrder] = useState("desc");
 
   useEffect(() => {
     setLoading(true);
-    getReviews({ limit: 6 * page }).then((res) => {
+    getReviews(formatQueries()).then((res) => {
       setReviews(res);
       setLoading(false);
     });
-  }, [page]);
+
+    function formatQueries() {
+      const query = { limit: 6 * page, sort_by: sort, order };
+
+      return query;
+    }
+  }, [page, order, sort]);
+
+  function flipHandler(e) {
+    e.preventDefault();
+    order === "asc" ? setOrder("desc") : setOrder("asc");
+  }
 
   return (
     <main>
-      <Filter />
+      <form onSubmit={(e) => flipHandler(e)}>
+        <Sort setSort={setSort} loading={loading} />
+        <button disabled={loading} type="submit">
+          <span class={`material-symbols-outlined`}>swap_vert</span>
+        </button>
+      </form>
       <Reviews reviews={reviews} />
       <p className={loading ? "" : utils.hidden}>Loading...</p>
       <button
